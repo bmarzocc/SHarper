0f1c59860ce23205f07465c10a5e89e53929d5c3
Badder <badder.marzocchi@cern.ch>
2023-03-08 08:35:43 +0100

    News samples for regressions

--- SHNtupliser/test/crab_cfg_reco.py
+++ SHNtupliser/test/crab_cfg_reco.py
@@ -7,7 +7,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_('General')
-config.General.requestName       = 'FourGammasGunPt1-100_DeepSC_algoD_idealICs'
+config.General.requestName       = 'FourElectronsGunPt1-500_Mustache_thres235fb'
 config.General.transferLogs      = True
 config.General.transferOutputs   = True
 
@@ -15,15 +15,15 @@ config.section_('JobType')
 config.JobType.pluginName        = 'Analysis'
 
 # Name of the CMSSW configuration file
-config.JobType.psetName          = 'egRegTreeMakerRefinedAOD_DeepSC_algoD_idealICs.py'
+config.JobType.psetName          = 'egRegTreeMakerRefinedAOD_Mustache_thres235fb.py'
 config.JobType.priority          = 30
 config.JobType.maxMemoryMB       = 3000
 config.JobType.numCores          = 2
 
 config.section_('Data')
 # This string determines the primary dataset of the newly-produced outputs.
-#config.Data.inputDataset         = '/FourElectronsGunPt1-100_pythia8_StdMix_Flat55To75_14TeV_112X_mcRun3_2021_realistic_v15/dpg_ecal-GEN-SIM-RAW-Reduced-f73d3671898879ae5e675fcbfe8fb906/USER'
-config.Data.inputDataset         = '/FourGammasGunPt1-100_pythia8_StdMix_Flat55To75_14TeV_112X_mcRun3_2021_realistic_v15/dpg_ecal-GEN-SIM-RAW-Reduced-f73d3671898879ae5e675fcbfe8fb906/USER'
+config.Data.inputDataset         = '/FourElectronsGunPt1-500_pythia8_PremixRun3_13p6TeV_122X_mcRun3_2021_realistic_v9_235fbNoise/dpg_ecal-GEN-SIM-RAW-CALO-6cb02bfc714e0c227af683c336cc381d/USER'
+#config.Data.inputDataset         = '/FourGammasGunPt1-500_pythia8_PremixRun3_13p6TeV_122X_mcRun3_2021_realistic_v9_235fbNoise/dpg_ecal-GEN-SIM-RAW-CALO-6cb02bfc714e0c227af683c336cc381d/USER'
 config.JobType.outputFiles       = ["egmRegTree.root"]
 config.Data.inputDBS             = 'global'   
 config.Data.inputDBS             = 'phys03'   
@@ -31,7 +31,7 @@ config.Data.splitting            = 'FileBased'
 config.Data.unitsPerJob          = 1
 config.Data.publication          = False
 config.Data.ignoreLocality       = True
-config.Data.outputDatasetTag     = 'RECO_UL18_pfRechitThres_DeepSC_algoD_idealICs'
+config.Data.outputDatasetTag     = 'RECO_Mustache_thres235fb'
 
 # This string is used to construct the output dataset name
 #config.Data.publishDataName = 'CRAB3-tutorial'
